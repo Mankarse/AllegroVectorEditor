693ae79602462202275492ce8ac581d9640c5336
Evan Wallace <onlyone@senet.com.au>
2012-08-03 18:41:28 +1000

    Added editable verticies

---
+++ AllegroVectorEditor/src/ave/vec/EditedVertex.h
@@ -0,0 +1,14 @@
+//
+//  EditedVertex.h
+//  AllegroVectorEditor
+//
+//  Created by Evan Wallace on 3/08/12.
+//  Copyright (c) 2012 Evan Wallace. All rights reserved.
+//
+
+#ifndef __AllegroVectorEditor__EditedVertex__
+#define __AllegroVectorEditor__EditedVertex__
+
+#include <iostream>
+
+#endif /* defined(__AllegroVectorEditor__EditedVertex__) */
