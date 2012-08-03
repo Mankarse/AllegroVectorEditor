bdd5c0a378f1d57a34c4566aaebfffb347ef6ef0
Evan Wallace <onlyone@senet.com.au>
2012-08-03 15:27:52 +1000

    Initial Commit

---
+++ AllegroVectorEditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AllegroVectorEditor
+//
+//  Created by Evan Wallace on 3/08/12.
+//  Copyright (c) 2012 Evan Wallace. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ AllegroVectorEditor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  AllegroVectorEditor
+//
+//  Created by Evan Wallace on 3/08/12.
+//  Copyright (c) 2012 Evan Wallace. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
