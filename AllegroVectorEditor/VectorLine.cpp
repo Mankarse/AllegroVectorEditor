9523c93720c7070cf2753a5f2d1a81def0db654a
Evan Wallace <onlyone@senet.com.au>
2012-08-03 17:56:14 +1000

    Even more crazy stuff

--- AllegroVectorEditor/VectorLine.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  VectorLine.cpp
-//  AllegroVectorEditor
-//
-//  Created by Evan Wallace on 3/08/12.
-//  Copyright (c) 2012 Evan Wallace. All rights reserved.
-//
-
-#include "VectorLine.h"
--- VectorColour.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  VectorColour.cpp
-//  AllegroVectorEditor
-//
-//  Created by Evan Wallace on 3/08/12.
-//  Copyright (c) 2012 Evan Wallace. All rights reserved.
-//
-
-#include "VectorColour.h"
